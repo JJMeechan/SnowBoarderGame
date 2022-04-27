8a5a2b2531e46a762342bc0c48fb421369359a91
john meechan <john2.meechan@live.uwe.ac.uk>
2022-04-27 17:48:34 +0100

    Particle systems
    Added particle systems that trigger on win and loss, as well as scene management that reloads the level

--- Snow Boarder/Assets/Scripts/crashDetector.cs
+++ Snow Boarder/Assets/Scripts/crashDetector.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class crashDetector : MonoBehaviour
 {
+
+    [SerializeField] float crashDelay;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
-            Debug.Log("Bonk");
+            crashEffect.Play();
+            Invoke("ReloadScene", crashDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Snow Boarder/Assets/Scripts/finishLine.cs
+++ Snow Boarder/Assets/Scripts/finishLine.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class finishLine : MonoBehaviour
 {
+
+    [SerializeField] float winDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            Debug.Log("You've finished!");
+            finishEffect.Play();
+            Invoke("ReloadScene", winDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
