84ca879c2ba9cc709bfcb19852cc1d7ee3ba9c5a
john meechan <john2.meechan@live.uwe.ac.uk>
2022-05-20 20:38:34 +0100

    Finished
    Project is now complete

--- Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float boostSpeed = 30f;
     [SerializeField] float baseSpeed = 20f;
     SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +22,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-        RespondToBoost();
+        if(canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
     }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+   
 
     void RespondToBoost()
     {
--- Snow Boarder/Assets/Scripts/crashDetector.cs
+++ Snow Boarder/Assets/Scripts/crashDetector.cs
@@ -9,10 +9,13 @@ public class crashDetector : MonoBehaviour
     [SerializeField] float crashDelay;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Ground")
+        if(collision.tag == "Ground" && hasCrashed == false)
         {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", crashDelay);
